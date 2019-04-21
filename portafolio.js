ef3c99962c2f97fa281a290cbff59d7a577a902b
Abril Araneda <49153827+abrilmayojunio@users.noreply.github.com>
2019-04-21 03:10:06 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -1,34 +1,51 @@
 // PRIMERO CREAMOS UNA VARIABLE QUE CONTIENE TODOS LOS DATOS DE TUS TRABAJOS
 var trabajos = [
 {
-    figure: "media/foto-01.jpg",
-    title: "Lorem ipsum 01", //javascript aquí puedo cambiar todo jejeje
-    about: "Nulla pretium non sapien eget fermentum",
+    figure: "media/cucho.gif",
+    title: "🐈🐀 Frugívoro", //javascript aquí puedo cambiar todo jejeje
+    about: "Helados de frutas, verduras y Cucho, un murciélago achorado come fruta. ",
 }, {
     figure: "media/foto-02.jpg",
-    title: "Lorem ipsum 02",
+    title: "🐈 Lorem ipsum 02",
     about: "Nulla pretium non sapien eget fermentum",
 }, {
-    figure: "media/foto-03.jpg",
-    title: "Lorem ipsum 03",
-    about: "Nulla pretium non sapien eget fermentum",
+    figure: "media/reta.gif",
+    title: "🐈🐀 RETA: Reciclaje Eficiente y Transporte de Agua",
+    about: "Sistema de reciclaje de agua en contexto de emergencia en el hogar." ,
+
+
 }, {
-    figure: "media/foto-04.jpg",
-    title: "Lorem ipsum 04",
-    about: "Nulla pretium non sapien eget fermentum",
+    figure: "media/filt04.gif",
+    title: "🐀 Práctica Profesional:FILT ",
+    about: "FILT; fibras libres transformadas, proyecto de economía circular textil, llevado a cabo por el equipo de diseño e investigación textil Open Textiles."
+  ,
 }, {
-    figure: "media/foto-05.jpg",
-    title: "Lorem ipsum 05",
-    about: "Nulla pretium non sapien eget fermentum",
+    figure: "media/veco1.png",
+    title: "🐈🐀 VECO",
+    about: "Red social comunitaria regulada, propia de cada comunidad de departamentos. VECO tiene como objetivo mantener en contacto, a los habitantes de estas comunidades, en situaciones de emergencia tanto como en la vida diaria.",
+
 }, {
-    figure: "media/foto-06.jpg",
-    title: "Lorem ipsum 06",
-    about: "Nulla pretium non sapien eget fermentum",
+    figure: "media/jop.gif",
+    title: "🐀 Jop ",
+    about: "Línea de ropa de bebés inspirada en perritos de todas las formas y clases.",
 }, {
     figure: "media/foto-07.jpg",
-    title: "Lorem ipsum 07",
+    title: "🐈 Lorem ipsum 07",
     about: "Nulla pretium non sapien eget fermentum",
+}, {
+  figure: "media/chana.gif",
+  title: "🐀 Accesorios Chana",
+  about: "Experimentación Textil con mezclilla, con el fin de lograr una nueva línea de productos para el emprendimiento de Roxana Escalona 'Accesorios Chana'. ",
+}, {
+  figure: "media/ilus.png",
+  title: "🐀 Ilustraciones & Dibujitos",
+  about: "Un parsín por ahí y por acá, me manejo en ilustraciones digitales tanto como análogas."
+},{
+  figure: "media/foto-06.jpg",
+  title: "🐈 Lorem ipsum 06",
+  about: "Nulla pretium non sapien eget fer",
 }
+
 ]
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
